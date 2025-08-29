979d18533299ce726f52bad6ccc66b6a00dca839
Eric Paul Michael Francis <eric.francis1103@gmail.com>
2025-08-28 21:02:19 -0500

    created and updated files

---
+++ samhain_apache.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+sudo yum update -y
+sudo yum install httpd -y
+sudo systemctl start httpd.service
+sudo systemctl enable httpd.service
+sudo echo "GREETINGS BOIL and GHOULS from $(hostname -f) at Spirits of Samhain!!!" > /var/www/html/index.html
