314b4fbc0ea8804bf28773a3cd570c560e3ac7e5
Eric Paul Michael Francis <eric.francis1103@gmail.com>
2025-08-30 23:11:52 -0500

    created plan1 and plan 1a

--- main.tf
+++ main.tf
@@ -242,9 +242,3 @@ resource "aws_lb_listener" "external-elb" {
   }
 }
 
-resource "datadog_monitor" "cpumonitor" {
-  name    = "cpu monitor"
-  type    = "metric alert"
-  message = "CPU usage alert"
-  query   = "avg(last_1m):avg:system.cpu.system{*} by {host} > 60"
-}
--- samhain_apache.sh
+++ samhain_apache.sh
@@ -6,4 +6,3 @@ sudo systemctl start httpd.service
 sudo systemctl enable httpd.service
 sudo echo "GREETINGS BOIL and GHOULS from $(hostname -f) at Spirits of Samhain!!!" > /var/www/html/index.html
 
-DD_API_KEY=3b787f4153fb7e23287bc9e7ad460abd992d8391 DD_AGENT_MAJOR_VERSION=7 bash -c "$(curl -L https://raw.githubusercontent.com/DataDog/datadog-agent/master/cmd/agent/install_script.sh)"
