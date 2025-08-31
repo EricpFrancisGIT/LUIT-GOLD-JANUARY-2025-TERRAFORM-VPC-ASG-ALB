86c51cea4946c8fe385e81b0a3188f5102a7a186
Eric Paul Michael Francis <eric.francis1103@gmail.com>
2025-08-30 21:10:07 -0500

    added datadog integrations

--- providers.tf
+++ providers.tf
@@ -4,10 +4,22 @@ terraform {
       source  = "hashicorp/aws"
       version = "~> 5.0"
     }
+    datadog = {
+      source  = "DataDog/datadog"
+      version = "3.72.0"
+    }
   }
 }
 
 # Configure the AWS Provider
 provider "aws" {
   region = "us-east-1"
+}
+
+provider "datadog" {
+  api_key = var.datadog_api_key
+  app_key = var.datadog_app_key
+  # Either of these forms works (pick one):
+  api_url = "https://api.${var.datadog_site}"
+  # or: site   = var.datadog_site
 }
